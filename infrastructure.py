225a6e0299bc6bcbadcc9a2a214ee5057b0bb2c1
Steven Braverman <steven@stevenbraverman.com>
2020-03-17 08:45:02 -0700

    DEVOPS-413 updates creditbureauapi to use format which allows specifying additional subnets. Current change should introduce no changes to the stack

--- infrastructure.py
+++ infrastructure.py
@@ -93,6 +93,12 @@ class APIVPC(VPC):
         },
         CREDIT_BUREAU_API: {
             SECOND_OCTET: VPC.get_second_octet(CREDIT_BUREAU_API, ENVIRONMENT),
+            PRIVATE_SUBNETS_CONFIG: {
+                '{0}A'.format(CREDIT_BUREAU_API): '10.{0}.10.0/24'.format(
+                    VPC.get_second_octet(CREDIT_BUREAU_API, ENVIRONMENT)),
+                '{0}B'.format(CREDIT_BUREAU_API): '10.{0}.11.0/24'.format(
+                    VPC.get_second_octet(CREDIT_BUREAU_API, ENVIRONMENT)),
+            }
         },
     }
 
